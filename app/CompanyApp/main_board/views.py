195305c18a1e9a31e31e79858191bc48b8b3838b
Golonka <kamil.golonka@nokia.com>
2021-03-24 20:36:57 +0100

    Add basic navbar macro

--- app/CompanyApp/main_board/views.py
+++ app/CompanyApp/main_board/views.py
@@ -5,4 +5,4 @@ main_board_bp = Blueprint("main_board", __name__, template_folder="templates")
 
 @main_board_bp.route("/")
 def index():
-    return render_template("main/index.html")
+    return render_template("main_board/board.html")
