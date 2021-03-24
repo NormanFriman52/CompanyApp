25cefbc71fc37dafbcc32eb31e9a54b086d42a87
Golonka <kamil.golonka@nokia.com>
2021-03-24 19:37:00 +0100

    Add data parser and arguments for user endpoints

--- app/CompanyApp/api/views.py
+++ app/CompanyApp/api/views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template, url_for, session, request
+from CompanyApp.data_parser import *
 
 api_bp = Blueprint("api", __name__, template_folder="templates")
 
 
 @api_bp.route("/")
 def api():
-    session["url"] = url_for("api.api")
     return 'api'
+
+
+@api_bp.route("/users")
+def user():
+    uid = request.args.get('uid')
+    if uid:
+        return get_single_user_params(uid)
+    else:
+        return get_users_params()
---
+++ app/CompanyApp/data_parser.py
@@ -0,0 +1,28 @@
+import json
+
+
+def get_json(json_path):
+    try:
+        with open(json_path) as json_file:
+            data = json.load(json_file)
+        return data
+    except Exception:
+        return {}
+
+
+def get_users_params(parse=True):
+    users = get_json(r'C:\Users\kgolonka\Desktop\home\ChatApp\CompanyApp\app\CompanyApp\DB\user_structure.json')
+    if parse:
+        return {"users": users}
+    return users
+
+
+def get_single_user_params(uid):
+    if isinstance(uid,str):
+        uid = int(uid)
+    users = get_users_params(parse=False)
+    for user in users:
+        if user["uid"] == uid:
+            return user
+    else:
+        return {}
