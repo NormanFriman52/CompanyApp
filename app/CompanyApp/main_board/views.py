b8ed16d7c2627943d75969537ff66d0a0b89bd81
Golonka <kamil.golonka@nokia.com>
2021-03-24 20:49:09 +0100

    Add Vue app  to main board

--- app/CompanyApp/main_board/views.py
+++ app/CompanyApp/main_board/views.py
@@ -5,4 +5,4 @@ main_board_bp = Blueprint("main_board", __name__, template_folder="templates")
 
 @main_board_bp.route("/")
 def index():
-    return render_template("main_board/board.html")
+    return render_template("main_board/board.html",url_for_api_messages=url_for('api.main_board'))
