d24ac7dafabeb3bf503bd7d0fee140802ac91973
Golonka <kamil.golonka@nokia.com>
2021-03-26 10:22:30 +0100

    Post and response for main board msg

--- app/CompanyApp/api/views.py
+++ app/CompanyApp/api/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, url_for, session, request
+from flask import Blueprint, render_template, url_for, session, request, redirect, jsonify
 from CompanyApp.data_parser import *
+from CompanyApp.controllers.main_board_messages_controller import insert_message
+from CompanyApp.main_board.forms import MessageForm
+from datetime import datetime
 
 api_bp = Blueprint("api", __name__, template_folder="templates")
 
@@ -21,3 +24,22 @@ def user():
 @api_bp.route("/main_board")
 def main_board():
     return get_main_board_massages()
+
+
+@api_bp.route("/main_board/send", methods=["POST"])
+def send():
+    payload = request.get_json()
+    if session['username']:
+        insert_msg = {
+            "body": payload.get("message"),
+            "date": f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+            "from": payload.get("from"),
+            "msgId": 3
+        }
+        insert_message(insert_msg)
+        content = {
+            "status": "OK"
+        }
+        resp = jsonify(content)
+        resp.status_code = 200
+        return resp
